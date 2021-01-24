6e68f4aee55f90a6da3003d0265175c0ede40c06
ShaneM123 <shanemoloneybusiness@gmail.com>
2021-01-24 18:29:11 +0100

    need to assert string correctly

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -7,9 +7,15 @@ fn main() {
     let m = App::from(yaml).get_matches();
     let mut kv_store = KvStore::new();
     match m.value_of("set") {
-        None => {panic!()}
-        Some(_val) => { kv_store.set("test".into(), "testing".into()); }
-    }
+        None => {}
+        Some(_val) => {         eprintln!("unimplemented");
+            panic!() }
+    };
+    match m.value_of("get") {
+        None => {}
+        Some(_val) => {         eprintln!("unimplemented");
+            panic!() }
+    };
     match m.value_of("v") {
         None => {}
         Some(_val) => {let version = env!("CARGO_PKG_VERSION");
--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,22 @@
 use std::collections::HashMap;
 
-pub struct KvStore{
-    //kv_db: HashMap<String, String>,
+pub struct KvStore {
+    kv_db: HashMap<String, String>,
 }
 
 impl KvStore {
-    pub fn new() -> KvStore {
+    pub fn new() -> KvStore{
         KvStore{
-           // kv_db: HashMap::new(),
+            kv_db: HashMap::new(),
         }
     }
-    pub fn set(&mut self, _key: String, _value: String) {
-       // self.kv_db.insert(key,value)
-        eprintln!("unimplemented");
-        panic!()
+    pub fn set(&mut self, key: String, value: String) -> Option<&String> {
+        self.kv_db.insert(key.clone(),value);
+        self.kv_db.get(&key)
     }
 
-    pub fn get(&self, _key: String)-> Option<String>{
-        eprintln!("unimplemented");
-        panic!()
+    pub fn get(&self, key: String) -> Option<&String> {
+        self.kv_db.get(&key)
     }
     pub fn remove(&self, _key: String)-> Option<String>{
         eprintln!("unimplemented");
