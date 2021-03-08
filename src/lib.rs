297055f152ceae53c9d5ceb62ed98cc512812af5
ShaneM123 <shanemoloneybusiness@gmail.com>
2021-03-08 20:37:20 +0100

    tests compile, 8 pass

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,26 @@
 use std::collections::HashMap;
+use std::path;
+use thiserror::Error;
+
+
+#[derive(Error, Debug)]
+pub enum KvsError {
+    #[error("unknown data store error")]
+    Unknown,
+}
+pub type Result<T> = std::result::Result<T,KvsError>;
 
 pub struct KvStore {
     kv_db: HashMap<String, String>,
 }
+
 
 impl Default for KvStore {
     fn default() -> Self {
         Self::new()
     }
 }
+
 
 impl KvStore {
     pub fn new() -> KvStore{
@@ -17,16 +29,24 @@ impl KvStore {
         }
     }
 
-    pub fn set(&mut self, key: String, value: String) {
-        self.kv_db.insert(key,value);
+    pub fn open(path: &path::Path) -> Result<KvStore> {
+
+        return Result::Ok(KvStore::new())
     }
 
-    pub fn get(&self, key: String) -> Option<String> {
-        self.kv_db.get(&key).cloned()
+    pub fn set(&mut self, key: String, value: String) -> Result<bool> {
+        self.kv_db.insert(key,value);
+        Ok(true)
     }
-    pub fn remove(&mut self, key: String) -> Option<String>{
+
+    pub fn get(&self, key: String) -> Result<Option<String>> {
+        Ok(self.kv_db.get(&key).cloned())
+
+    }
+    pub fn remove(&mut self, key: String) -> Result<Option<String>> {
         self.kv_db.remove(&key);
         self.get(key)
     }
+
 }
 
