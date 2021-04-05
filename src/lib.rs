41cdab0947108158c65a8912f8d835be1511614d
ShaneM123 <shanemoloneybusiness@gmail.com>
2021-04-05 16:43:49 +0200

    messing with file storage options

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
 use std::collections::HashMap;
 use std::path;
+use std::fs;
 use std::fmt::Formatter;
+use std::fs::OpenOptions;
+use std::path::Path;
+use std::io::Write;
 
 /// WordCountError enumerates all possible errors returned by this library.
 #[derive(Debug, Clone)]
@@ -26,6 +30,7 @@ pub type Result<T> = std::result::Result<T,KvsError>;
 
 pub struct KvStore {
     kv_db: HashMap<String, String>,
+    path: String,
 }
 
 
@@ -40,16 +45,35 @@ impl KvStore {
     pub fn new() -> KvStore{
         KvStore{
             kv_db: HashMap::new(),
+            path: "".to_string(),
         }
     }
-
+    pub fn new_with_path(path: &path::Path) -> KvStore{
+        KvStore{
+            kv_db: HashMap::new(),
+            path: path.to_str().unwrap().parse().unwrap()
+        }
+    }
+    // fn open_existing_file_or_create(path: &PathBuf) -> File {
+    //     fs::create_dir_all(path.parent().unwrap());
+    //
+    //     OpenOptions::new()
+    //         .append(true)
+    //         .create(true)
+    //         .read(true)
+    //         .open(path)
+    //         .expect("could not append file")
+    // }
     pub fn open(path: &path::Path) -> Result<KvStore> {
+        fs::create_dir_all(&path);
 
-        return Result::Ok(KvStore::new())
+        return Result::Ok(KvStore::new_with_path(path))
     }
 
     pub fn set(&mut self, key: String, value: String) -> Result<bool> {
-        self.kv_db.insert(key,value);
+        self.kv_db.insert(key.clone(),value);
+        let mut file = fs::File::open(&self.path).unwrap();
+        file.write(self.kv_db.get(&key).unwrap().as_bytes());
         Ok(true)
     }
 
