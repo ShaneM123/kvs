20205705738e72aced0955d077c71ee305cd8ade
ShaneM123 <shanemoloneybusiness@gmail.com>
2021-06-01 19:35:12 +0200

    some half baked match on args

--- src/bin/kvs-client.rs
+++ src/bin/kvs-client.rs
@@ -1,5 +1,5 @@
 
-use clap::{App, Arg, SubCommand};
+use clap::{App, Arg, SubCommand, ArgMatches};
 use std::env::{args, Args};
 use std::net::TcpStream;
 use std::io::Error;
@@ -11,21 +11,34 @@ pub fn main() {
         .about("a db server following pingcap talent plan")
         .subcommands(vec![
             SubCommand::with_name("set")
-                .args(&[Arg::with_name("KEY").required(true),Arg::with_name("VALUE").required(true),])
+                .args(&[Arg::with_name("KEY").required(true),
+                    Arg::with_name("VALUE").required(true),
+                    Arg::with_name("addr")
+                        .required(true)
+                        .value_name("addr")
+                        .long("addr")
+                        .takes_value(true),
+                ])
             ,
             SubCommand::with_name("get")
-                .arg(Arg::with_name("KEY").required(true)),
-                ]
-        )
-        .arg(
+                .args(&[Arg::with_name("KEY").required(true),
             Arg::with_name("addr")
                 .required(true)
                 .value_name("addr")
                 .long("addr")
-                .takes_value(true)
+                .takes_value(true),
+                ])]
         )
+        // .arg(
+        //
+        // )
         .get_matches();
-    let address = matches.value_of("addr").unwrap();
+    let address = match matches.subcommand_matches("set"){
+        None => {
+            matches.subcommand_matches("get").unwrap().value_of("addr").unwrap()
+        }
+        Some(val) => {val.value_of("addr").unwrap()}
+    };
     let stream = TcpStream::connect(address);
     match stream {
         Ok(val) => { println!("connected to server ")}
